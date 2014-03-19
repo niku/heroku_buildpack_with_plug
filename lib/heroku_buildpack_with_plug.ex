b36eafcabc03790889a1395ea5375c561c4f2f6c
niku <niku@niku.name>
2014-03-19 19:53:39 +0900

    Hello world

--- lib/heroku_buildpack_with_plug.ex
+++ lib/heroku_buildpack_with_plug.ex
@@ -1,2 +1,18 @@
 defmodule HerokuBuildpackWithPlug do
+  import Plug.Connection
+
+  def init(options) do
+    # initialize options
+
+    options
+  end
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello world")
+  end
 end
+
+IO.puts "Running HerokuBuildpackWithPlug with Cowboy on http://localhost:4000"
+Plug.Adapters.Cowboy.http HerokuBuildpackWithPlug, []
